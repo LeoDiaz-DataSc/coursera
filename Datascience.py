c53ac56448ebeef8bc882c8368eb803cd09fe582
LeoDiaz-DataSc <parlepeldido@gmail.com>
2024-09-30 18:41:46 -0600

    Add files via upload

---
+++ Datascience.py
@@ -0,0 +1,47 @@
+print("# Mi Cuaderno de Ciencia de Datos")
+print("## Introducción")
+print("Este cuaderno explorará los fundamentos de la ciencia de datos, desde los lenguajes y bibliotecas hasta ejemplos prácticos.")
+
+print(" Bibliotecas de Ciencia de Datos")
+print("* **Pandas:** Para manipulación y análisis de datos.")
+print("* **NumPy:** Para operaciones numéricas y matrices.")
+print("* **Matplotlib:** Para visualización de datos.")
+print("* **Scikit-learn:** Para aprendizaje automático.")
+
+
+print("### Herramientas de Ciencia de Datos")
+print("| Herramienta | Descripción |")
+print("|---|---|")
+print("| Jupyter Notebook | Entorno interactivo para programación y análisis de datos |")
+print("| RStudio | Entorno de desarrollo integrado para R |")
+print("| Google Colab | Entorno de programación gratuito basado en la nube |")
+
+
+print("### Ejemplos de Expresiones Aritméticas")
+print("* Suma: 2 + 3 = 5")
+print("* Resta: 10 - 4 = 6")
+print("* Multiplicación: 5 * 2 = 10")
+print("* División: 15 / 3 = 5")
+
+
+numero1 = 5
+numero2 = 3
+resultado_multiplicacion = numero1 * numero2
+resultado_suma = numero1 + numero2
+print("El resultado de la multiplicación es:", resultado_multiplicacion)
+print("El resultado de la suma es:", resultado_suma)
+
+
+minutos = 120
+horas = minutos / 60
+print(minutos, "minutos equivalen a", horas, "horas")
+
+
+objetivos = ["Exploración de datos", "Visualización de datos", "Modelado predictivo", "Deployment"]
+print("Objetivos del proyecto:")
+for objetivo in objetivos:
+    print("- " + objetivo)
+
+# Ejercicio 11: Nombre del autor (Markdown)
+print("Trabajo realiazo por: ")
+print("Diego Leobardo diaz hernandez")
